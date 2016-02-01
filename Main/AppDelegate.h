a617c5d795c21770aea110bca11a4607d9a208a7
Qiankun Zhuang <qiankunzhuang@QiankundeMac-Pro.local>
2016-02-01 10:14:10 -0800

    Initial Commit

---
+++ Main/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Main
+//
+//  Created by Qiankun Zhuang on 16-2-1.
+//  Copyright (c) 2016年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Main/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Main
+//
+//  Created by Qiankun Zhuang on 16-2-1.
+//  Copyright (c) 2016年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
