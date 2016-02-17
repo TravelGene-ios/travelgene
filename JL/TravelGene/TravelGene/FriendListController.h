18b29cdf0da568668c1bb47e45807a4eeff9f72c
TinaLi <jli3@andrew.cmu.edu>
2016-02-16 22:57:47 -0500

    commit

---
+++ JL/TravelGene/TravelGene/FriendListController.h
@@ -0,0 +1,13 @@
+//
+//  FriendListController.h
+//  TravelGene
+//
+//  Created by Tina on 2/16/16.
+//  Copyright © 2016 TravelGene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendListController : UIViewController
+
+@end
