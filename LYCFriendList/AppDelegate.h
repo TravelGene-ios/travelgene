44838bc585178aab73790da7a63a4323044a3806
yanchenl <yanchenl@andrew.cmu.edu>
2016-02-17 16:46:46 -0500

    Initial Commit

---
+++ LYCFriendList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYCFriendList
+//
+//  Created by Charles on 2/17/16.
+//  Copyright © 2016 TravelGene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYCFriendList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYCFriendList
+//
+//  Created by Charles on 2/17/16.
+//  Copyright © 2016 TravelGene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
