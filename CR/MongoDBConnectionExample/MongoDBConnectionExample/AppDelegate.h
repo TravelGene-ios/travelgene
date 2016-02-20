e5c5074487528df7c61b35a99fda4ee5982631d8
Tocat <c.ran@msn.com>
2016-02-19 20:31:38 -0500

    add MongoDBConnectionExample

---
+++ CR/MongoDBConnectionExample/MongoDBConnectionExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MongoDBConnectionExample
+//
+//  Created by Tony on 16/2/19.
+//  Copyright (c) 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CR/MongoDBConnectionExample/MongoDBConnectionExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MongoDBConnectionExample
+//
+//  Created by Tony on 16/2/19.
+//  Copyright (c) 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
