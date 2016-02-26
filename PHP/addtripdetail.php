f2b46960760c3a8c6298d6e5c97c3b4367d3e7b7
yanchenl <yanchenl@andrew.cmu.edu>
2016-02-26 16:40:19 -0500

    fix php bug and give example

--- PHP/addtripdetail.php
+++ PHP/addtripdetail.php
@@ -12,7 +12,9 @@ $date_end = $_GET['date_end'];
 $destination = $_GET['destination'];
 $activity = $_GET['activity'];
 
-$sql = "INSERT INTO tripdetail (email, date_start, date_end, destination, activity) VALUES('$email','$date_start','$date_end','$destination','$activity');
+$sql = "INSERT INTO tripdetail (email, date_start, date_end, destination, activity) VALUES('$email','$date_start','$date_end','$destination','$activity')";
+echo $sql;
+//http://ec2-52-90-95-189.compute-1.amazonaws.com/addtripdetail.php?email=gmail&date_start=2012-02-02&date_end=2012-02-04&destination=NewYork.NY&activity={%22activity%22:[%22spot1%22,%22spot2%22]}
 
 $retval = mysql_query( $sql, $conn );
 if(!$retval) {
